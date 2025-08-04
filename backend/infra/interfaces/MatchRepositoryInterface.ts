30375ca108300277b2945c4ccc2d9e7b0c6e4f3f
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-08-04 17:17:43 -0300

    match interface

---
+++ backend/infra/interfaces/MatchRepositoryInterface.ts
@@ -0,0 +1,10 @@
+import { Match } from "entities/game/Match";
+
+export interface MatchRepositoryInterface {
+  create(match: Match): Promise<Match>;
+  findById(id: string): Promise<Match | null>;
+  findAll(): Promise<Match[]>;
+  update(match: Match): Promise<Match>;
+  delete(id: string): Promise<boolean>;
+  findByPlayerId(playerId: string): Promise<Match | null>;
+}
