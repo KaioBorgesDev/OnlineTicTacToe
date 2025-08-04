ab7d9ca5d371a2d59b234f54392d2b839029f9c7
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-08-04 16:07:31 -0300

    Object Calistenic's in Player

--- backend/entities/Player.ts
+++ backend/entities/Player.ts
@@ -1,59 +1,32 @@
-export class Player {
-    public readonly _id: string;
-    public name: string;
-    private isConnected: boolean;
-    private currentGameID?: string;
-    private wins: number;
-    private losses: number;
-    private draws: number;
+import { GameSession } from "./GameSession";
+import { GameStatistics } from "./GameStatistics";
+import { User } from "./User";
+
+export class Player extends User {
+
+    private gameStatistics: GameStatistics;
+    private gameSession: GameSession;
 
     constructor(_id: string, name: string = `Player-${_id.slice(0, 6)}`){
-        this._id = _id;
-        this.name = name;
-        this.isConnected = true;
-        this.wins = 0;
-        this.losses = 0;
-        this.draws = 0;
-    }
-
-    public joinGame(gameId: string){
-        this.currentGameID = gameId;
-    }
-    public leaveGame(){
-        this.currentGameID = undefined;
-    }
-    public connect(){
-        this.isConnected = true;
-    }
-    public disconnect(){
-        this.isConnected = false;
-    }
-    public addWin(){
-        this.wins += 1;
-    }
-    public addLoss(){
-        this.losses += 1;
-    }
-    public addDraw(){
-        this.draws += 1;
-    }
-    public getStatus(){
-        return {
-            wins: this.wins,
-            losses: this.losses,
-            draws: this.draws,
-            totalGames: this.wins + this.losses + this.draws
-        }
+        super(_id, name);
+        this.gameSession = new GameSession();
+        this.gameStatistics = new GameStatistics();
     }
 
     public getPublicProfile(){
         return {
             _id: this._id,
             name: this.name,
-            isConnected: this.isConnected,
-            currentGameID: this.currentGameID,
-            ...this.getStatus()
+            ...this.gameSession.getStatus(),
+            ...this.gameStatistics.getStats()
         }
     }
 
+    public getGameSessionStatus() {
+        return this.gameSession;
+    }
+
+    public getStatistics() {
+        return this.gameStatistics;
+    }
 }
